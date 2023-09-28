0aff427ac24a0b36b0d0b707c76c2c84ce8f4abe
Theodor Philippov <115158972+ravenMVA@users.noreply.github.com>
2023-09-28 23:13:10 +0300

    Update lab21_script.sh

--- lab21_script.sh
+++ lab21_script.sh
@@ -1,42 +1,78 @@
 #!/bin/bash
 
-if [$# -ne 4]; then #проверяем, что передано необходимое количество аргументов 
-echo "Using: $0 <directory> <suffix> <minimum length> <file to add path>"
-exit 1
+# Проверяем, что передано два параметра
+if [ $# -ne 2 ]; then
+    echo "Использование: $0 <суффикс> <длина[байт]>"
+    exit 1
 fi
-#извлекаем аргументы 
-directory="$1"
-suffix ="$2"
-min_length= "$3"
-file_to_add= "$4"
 
-#проверяем, что директория существует 
-if 
-    [! -d "$directory"]; then
-    echo  "Directory $directory does not exist."
-    exit 1
- fi 
+# Извлекаем параметры
+suff="$1"
+length="$2"
 
- #проверяем, что файл добавления существует 
- if 
-    [! -f "$file_to_add"]; then
-    echo "File to add $file_to_add does not exist."
-    exit 1
- fi
+# Выводим параметры
+echo "Расширение файлов: $suff"
+echo "Желаемый размер: $length байт"
 
-#проходимся по файлам в директории с указанным суффиксом 
-for file in "$directory"/*"$suffix"; do 
-#проверяем, является ли файл меньше указанной длины
-    file_length= $(wc -c <"$file")
-        if [ "$file_length" -lt "$min_length"]; then
-        #вычисляем, cколько копий нужно добавить, чтобы получить заданную длину
-            num_copies=$((($min_length - $file_length + $(wc -c <"$file_to_add") - 1)/$ (wc -c <"$file_to_add"))
-    #добавляем необходимое количество копий 
-        for (i=0; i < num_copies; i++) do
-            cat "$file_to_add" >> "$file"
-        done 
-    echo "File '$file' was extended for $min_length bytes."
-   else 
-   echo "File $file has already required length."
-   fi
+echo "Найденные файлы:"
+file_list=$(find . -type f -name "*.$suff")
+files_to_increase=()
+
+# Смотрим, какие файлы подходят для увеличения
+for file_path in ${file_list[@]}; do
+    file=$(basename "$file_path")
+    file_size=$(stat -f%z "$file")
+    echo "$file [$file_size байт]"
+    if [ "$file_size" -lt "$length" ]; then
+        files_to_increase+=("$file_path")
+    fi
 done
+
+# Получаем путь файла, который следует прибавить к остальным
+read -r -p "Введите путь к файлу, который следует использовать для заполнения: " add_file_path
+
+# Проверка на то, есть ли этот файл в списке найденных
+for file_path in ${file_list[@]}; do
+    if [ "$(realpath $file_path)" = "$(realpath $add_file_path)" ]; then
+        echo "Нельзя использовать файлы из списка"
+        exit 1
+    fi
+done
+
+# Существует ли файл
+if [ -f "$add_file_path" ]; then
+    if [ -s "$add_file_path" ]; then
+        file_size=$(stat -f%z "$add_file_path")
+        echo "Файл существует: $add_file_path [$file_size байт]"
+    else
+        echo "Размер указанного файла равен 0 байт"
+        exit 2
+    fi
+else
+    echo "Введенный путь неверен: $add_file_path"
+    exit 3
+fi
+
+# Пробегаемся по файлам
+for file_path in ${files_to_increase[@]}; do
+    # Увеличиваем до тех пор, пока размер не станет >= $length
+    while true; do
+        file_size=$(stat -f%z "$file_path")
+
+        if [ "$file_size" -lt "$length" ]; then
+            cat "$add_file_path" >> "$file_path"
+        else
+            break 
+        fi
+    done
+done
+
+# Вывод измененных файлов с их новыми размерами
+echo "Размер файлов увеличен:"
+for file_path in ${files_to_increase[@]}; do
+    file=$(basename "$file_path")
+    file_size=$(stat -f%z "$file")
+    echo "$file [$file_size байт]"
+done
+
+exit 0
