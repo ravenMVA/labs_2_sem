c9bac74af3d0776b57976125f212d9267a5a34d6
Theodor Philippov <115158972+ravenMVA@users.noreply.github.com>
2023-10-17 20:33:50 +0300

    Update main.c

--- KP8/main.c
+++ KP8/main.c
@@ -10,11 +10,12 @@ int main() {
         double value;
         int choice = 0;
     	printf("    1. Добавить элемент\n");
-        printf("    2. Удалить элемент\n");
-        printf("    3. Вывести список\n");
-        printf("    4. Найти длину списка\n");
-        printf("    5. Переставить список в обратном порядке\n");
-        printf("    6. Выйти\n");
+        printf("    2. Вставить по индексу\n");
+        printf("    3. Удалить элемент\n");
+        printf("    4. Вывести список\n");
+        printf("    5. Найти длину списка\n");
+        printf("    6. Переставить список в обратном порядке\n");
+        printf("    7. Выйти\n");
 
         scanf("%d", &choice);
 
@@ -26,24 +27,33 @@ int main() {
                 printf("Элемент добавлен\n");
                 break;
             case 2:
+                printf("Введите индекс: ");
+                int index;
+                scanf("%d", &index);
+                printf("Введите значение добавляемого элемента: ");
+        		scanf("%lf", &value);
+       	 		insert(l, index, value);
+                printf("Элемент добавлен\n");
+                break;
+            case 3:
                 printf("Введите номер элемента: ");
                 int num;
         		scanf("%d", &num);
         		remove_node(l, num - 1);
                 printf("Элемент удален\n");
                 break;
-            case 3:
+            case 4:
                 printf("Список: ");
                 print_list(l);
                 break;
-            case 4:
+            case 5:
                 printf("Длина списка: %d\n", length(l));
                 break;
-            case 5:
+            case 6:
                 reverse_list(l);
                 printf("Список развернут\n");
                 break;
-            case 6:
+            case 7:
                 free_list(l);
     			return 0;
                 break;
